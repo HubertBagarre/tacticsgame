83f994a2cc1c5d354f7a8b8ec10d3c2627ad4349
Hub <h.ramiandrasoa@rubika-edu.com>
2023-09-07 18:17:38 +0200

    Tile generator connects neighbors

--- Tactics/Assets/Scripts/TileGenerator.cs
+++ Tactics/Assets/Scripts/TileGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,21 +9,50 @@ public class TileGenerator : MonoBehaviour
     [SerializeField] private Vector2Int gridSize;
     [SerializeField] private Vector2 tileSpacing;
     [SerializeField] private List<Tile> tiles;
+    private Tile[,] grid;
 
     [ContextMenu("Generate Grid")]
     public void GenerateGrid()
     {
         ClearTiles();
         tiles = new List<Tile>();
+        grid = new Tile[gridSize.x,gridSize.y];
+        
         for (int y = 0; y < gridSize.y; y++)
         {
-             for (int x = 0; x < gridSize.x; x++) 
-             {
+            for (int x = 0; x < gridSize.x; x++)
+            {
+
                 var pos = new Vector3(x * tileSpacing.x, 0, y * tileSpacing.y);
                 var tile = Instantiate(tilePrefab, pos, Quaternion.identity, transform);
                 tile.name = $"Tile {x},{y}";
-                tiles.Add(tile); 
-             }
+                tile.InitPosition(x,y);
+                tiles.Add(tile);
+                grid[x,y] = tile;
+            }
+        }
+
+        foreach (var tile in tiles)
+        {
+            var x = tile.Position.x;
+            var y = tile.Position.y;
+            var neighbors = new Tile[8];
+
+            bool top = y == gridSize.y - 1;
+            bool bot = y == 0;
+            bool right = x == gridSize.x - 1;
+            bool left = x == 0;
+            
+            if(!top) neighbors[0] = grid[x,y + 1];
+            if(!right && !top) neighbors[1] = grid[x + 1,y + 1];
+            if(!right) neighbors[2] = grid[x + 1,y];
+            if(!right && !bot) neighbors[3] = grid[x + 1,y - 1];
+            if(!bot) neighbors[4] = grid[x,y - 1];
+            if(!left && !bot) neighbors[5] = grid[x - 1,y - 1];
+            if(!left) neighbors[6] = grid[x - 1,y];
+            if(!left && !top) neighbors[7] = grid[x - 1,y + 1];
+            
+            tile.InitNeighbors(neighbors);
         }
     }
     
--- Tactics/Assets/Scripts/Tiles/Tile.cs
+++ Tactics/Assets/Scripts/Tiles/Tile.cs
@@ -1,26 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Tile : MonoBehaviour
 {
+    [SerializeField] private Vector2Int position;
+    public Vector2Int Position => position;
     [SerializeField] private Tile[] neighbors; //0 is top, then clockwise
 
-    [SerializeField] private Unit unit;
+    [SerializeField] private Unit currentUnit;
 
-    public void SetNeighbors(Tile[] tiles)
+    public void InitPosition(int x,int y)
+    {
+        position = new Vector2Int(x,y);
+    }
+    
+    public void InitNeighbors(Tile[] tiles)
     {
         neighbors = tiles;
     }
-
+    
+    public void SetUnit(Unit unit)
+    {
+        currentUnit = unit;
+        //probably some callbacks
+    }
+    
     public bool HasUnit()
     {
-        return unit != null;
+        return currentUnit != null;
     }
     
     public Unit GetUnit()
     {
-        return unit;
+        return currentUnit;
     }
     
     public Tile[] GetNeighbors(bool includeDiag = false)
