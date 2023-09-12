d409a6cd07701e06d5b6dab7de1c8274ab72d60b
Hub <h.ramiandrasoa@rubika-edu.com>
2023-09-12 18:25:24 +0200

    Added Ephemeral events (gets removed from list after being invoked)

--- Tactics/Assets/Scripts/Managers/EventManager.cs
+++ Tactics/Assets/Scripts/Managers/EventManager.cs
@@ -1,17 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class EventManager : MonoBehaviour
 {
     private static EventManager Instance;
-    
+
     private void Awake()
     {
         if (Instance != null)
         {
-            Debug.LogWarning($"{Instance} is already an instance of {typeof(EventManager)}",Instance);
+            Debug.LogWarning($"{Instance} is already an instance of {typeof(EventManager)}", Instance);
             Destroy(this);
             return;
         }
@@ -20,10 +21,11 @@ public class EventManager : MonoBehaviour
     }
 
     private readonly Dictionary<Type, List<Delegate>> m_events = new();
+    private readonly List<Delegate> ephemeral_events = new();
 
     public delegate void EventCallback<in T>(T data);
 
-    public static void AddListener<T>(EventCallback<T> callback)
+    public static void AddListener<T>(EventCallback<T> callback, bool ephemeral = false)
     {
         if (Instance.m_events.TryGetValue(typeof(T), out var listeners))
         {
@@ -34,9 +36,11 @@ public class EventManager : MonoBehaviour
         }
         else
         {
-            listeners = new List<Delegate> { callback };
+            listeners = new List<Delegate> {callback};
             Instance.m_events.Add(typeof(T), listeners);
         }
+
+        if (ephemeral) Instance.ephemeral_events.Add(callback);
     }
 
     public static void RemoveListener<T>(EventCallback<T> callback)
@@ -44,25 +48,41 @@ public class EventManager : MonoBehaviour
         if (!Instance.m_events.TryGetValue(typeof(T), out var listeners)) return;
 
         if (listeners.Contains(callback)) listeners.Remove(callback);
+        if (Instance.ephemeral_events.Contains(callback)) Instance.ephemeral_events.Remove(callback);
     }
 
     public static void RemoveListeners<T>()
     {
         if (!Instance.m_events.TryGetValue(typeof(T), out var listeners)) return;
 
+        foreach (var callback in listeners.Where(callback => Instance.ephemeral_events.Contains(callback)))
+        {
+            Instance.ephemeral_events.Remove(callback);
+        }
+
         Instance.m_events.Remove(typeof(T));
     }
 
     public static void Trigger<T>(T triggeredKey)
     {
-        if (!Instance.m_events.TryGetValue(typeof(T), out var listeners))
-        {
-            return;
-        }
+        if (!Instance.m_events.TryGetValue(typeof(T), out var listeners)) return;
 
+        var listenersToRemove = new List<Delegate>();
         foreach (var listener in listeners.ToArray())
         {
             listener.DynamicInvoke(triggeredKey);
+
+            if (Instance.ephemeral_events.Contains(listener))
+            {
+                Instance.ephemeral_events.Remove(listener);
+                listenersToRemove.Add(listener);
+            }
         }
+
+        foreach (var listener in listenersToRemove)
+        {
+            listeners.Remove(listener);
+        }
+        listenersToRemove.Clear();
     }
-}   
+}
