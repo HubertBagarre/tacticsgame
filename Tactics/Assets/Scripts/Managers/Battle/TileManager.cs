421de5fa621ac65ff499e3dac57b52794ae81279
Hub <h.ramiandrasoa@rubika-edu.com>
2023-09-12 19:51:39 +0200

    Move player movement logic from tilemanager to playerbehaviour

--- Tactics/Assets/Scripts/Managers/Battle/TileManager.cs
+++ Tactics/Assets/Scripts/Managers/Battle/TileManager.cs
@@ -20,24 +20,14 @@ namespace Battle
         [Header("Debug")] [SerializeField] private List<Tile> tiles = new List<Tile>();
 
         public List<Tile> AllTiles => tiles.ToList();
-
-        public List<Tile> SelectableTiles { get; private set; } = new List<Tile>();
-
+        
         private void Start()
         {
             EventManager.AddListener<EndUnitTurnEvent>(ClearSelectableTilesOnTurnEnd);
-
-            EventManager.AddListener<UnitMovementEndEvent>(UpdateAvailableUnitMovementTilesAfterMovementEnd);
-            EventManager.AddListener<UnitMovementStartEvent>(ClearSelectableTilesOnMovementStart);
-
+            
             void ClearSelectableTilesOnTurnEnd(EndUnitTurnEvent _)
             {
-                ClearSelectableTiles();
-            }
-
-            void ClearSelectableTilesOnMovementStart(UnitMovementStartEvent _)
-            {
-                ClearSelectableTiles();
+                ResetTileAppearance();
             }
         }
 
@@ -46,121 +36,20 @@ namespace Battle
             tiles = list;
         }
 
-        private Tile GetClickTile()
+        public Tile GetClickTile()
         {
             InputManager.CastCamRay(out var tileHit, worldLayers);
 
             return tileHit.transform != null ? tileHit.transform.GetComponent<Tile>() : null;
         }
 
-        private void ClearSelectableTiles()
+        private void ResetTileAppearance()
         {
-            SelectableTiles.Clear();
-
             foreach (var tile in tiles)
             {
                 tile.SetAppearance(Tile.Appearance.Default);
                 tile.SetPathRing(0);
             }
-        }
-        
-        private void UpdateAvailableUnitMovementTilesAfterMovementEnd(UnitMovementEndEvent ctx)
-        {
-            UpdateAvailableUnitMovementTiles(ctx.Unit);
-        }
-
-        public void UpdateAvailableUnitMovementTiles(Unit unit)
-        {
-            ClearSelectableTiles();
-            
-            if (!unit.CanMove) return;
-            if (unit.MovementLeft <= 0) return;
-
-            SetSelectableTilesForMovement(unit.Tile, unit.MovementLeft, false, unit.Stats.WalkableTileSelector);
-
-            InputManager.LeftClickEvent += MoveUnitOnClick;
-
-            void MoveUnitOnClick()
-            {
-                MoveUnit(unit);
-
-                InputManager.LeftClickEvent -= MoveUnitOnClick;
-            }
-        }
-
-        private void SetSelectableTilesForMovement(Tile origin, int range, bool includeDiag,
-            Func<Tile, bool> extraCondition = null)
-        {
-            extraCondition ??= _ => true;
-
-            SelectableTiles.Add(origin);
-            var justAdded = new List<Tile>() {origin};
-            var iteration = 0;
-
-            origin.SetPathRing(iteration);
-
-            AddNeighbors();
-
-            foreach (var tile in SelectableTiles)
-            {
-                tile.SetAppearance(Tile.Appearance.Selectable);
-            }
-
-            void AddNeighbors()
-            {
-                if (iteration >= range) return;
-
-                var neighbors = justAdded.SelectMany(tile => tile.GetDirectNeighbors(includeDiag)).Distinct().ToList();
-
-                var validTiles = neighbors
-                    .Where(tile => !SelectableTiles.Contains(tile))
-                    .Where(extraCondition)
-                    .ToList();
-
-                SelectableTiles.AddRange(validTiles);
-                justAdded = validTiles;
-
-                iteration++;
-
-                foreach (var addedTile in justAdded)
-                {
-                    addedTile.SetPathRing(iteration);
-                }
-
-                AddNeighbors();
-            }
-        }
-
-        private void MoveUnit(Unit unit)
-        {
-            var destination = GetClickTile();
-
-            if (!SelectableTiles.Contains(destination)) return;
-
-            var path = GetPathFromSelectableTiles(destination);
-
-            ClearSelectableTiles();
-
-            unit.MoveUnit(path);
-        }
-
-        public List<Tile> GetPathFromSelectableTiles(Tile destination)
-        {
-            if (!SelectableTiles.Contains(destination)) return null;
-            
-            var path = new List<Tile>() {destination};
-            var lastAdded = destination;
-
-            for (int i = destination.PathRing - 1; i >= 1; i--)
-            {
-                lastAdded = lastAdded.GetDirectNeighbors().FirstOrDefault(tile => tile.PathRing == i);
-                if (lastAdded == null) break;
-                path.Add(lastAdded);
-            }
-
-            path.Reverse();
-
-            return path;
         }
     }
 }
--- Tactics/Assets/Scripts/ScriptableObjects Scripts/UnitBehaviourSOs/PlayerUnitBehaviourSO.cs
+++ Tactics/Assets/Scripts/ScriptableObjects Scripts/UnitBehaviourSOs/PlayerUnitBehaviourSO.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Battle;
 using Battle.UIEvents;
 using Battle.UnitEvents;
@@ -6,9 +9,14 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "UnitBehaviour/PlayerUnit")]
 public class PlayerUnitBehaviourSO : UnitBehaviourSO
 {
+    private List<Tile> selectableTilesForMovement = new List<Tile>();
+    
     public override void InitBehaviour(Unit unit)
     {
         EventManager.AddListener<EndUnitTurnEvent>(EndPlayerControl);
+        
+        EventManager.AddListener<UnitMovementStartEvent>(ClearSelectableTilesOnMovementStart);
+        EventManager.AddListener<UnitMovementEndEvent>(UpdateAvailableUnitMovementTilesAfterMovementEnd);
         
         void EndPlayerControl(EndUnitTurnEvent ctx)
         {
@@ -16,13 +24,127 @@ public class PlayerUnitBehaviourSO : UnitBehaviourSO
             
             EventManager.Trigger(new EndPlayerControlEvent());
         }
+        
+        void ClearSelectableTilesOnMovementStart(UnitMovementStartEvent _)
+        {
+            ResetTilesAppearance();
+        }
+        
+        void UpdateAvailableUnitMovementTilesAfterMovementEnd(UnitMovementEndEvent ctx)
+        {
+            UpdateAvailableUnitMovementTiles(ctx.Unit);
+        }
     }
 
     public override void RunBehaviour(Unit unit)
     {
-        tileM.UpdateAvailableUnitMovementTiles(unit);
+        UpdateAvailableUnitMovementTiles(unit);
         
         EventManager.Trigger(new StartPlayerControlEvent());
+    }
+    
+    private void UpdateAvailableUnitMovementTiles(Unit unit)
+    {
+        ResetTilesAppearance();
+        
+        selectableTilesForMovement.Clear();
+            
+        if (!unit.CanMove) return;
+        if (unit.MovementLeft <= 0) return;
+
+        SetSelectableTilesForMovement(unit.Tile, unit.MovementLeft, false, unit.Stats.WalkableTileSelector);
+
+        InputManager.LeftClickEvent += MoveUnitOnClick;
+
+        void MoveUnitOnClick()
+        {
+            MoveUnit(unit);
+
+            InputManager.LeftClickEvent -= MoveUnitOnClick;
+        }
+    }
+    
+    private void ResetTilesAppearance()
+    {
+        foreach (var tile in tileM.AllTiles)
+        {
+            tile.SetAppearance(Tile.Appearance.Default);
+            tile.SetPathRing(0);
+        }
+    }
+    
+    private void SetSelectableTilesForMovement(Tile origin, int range, bool includeDiag, Func<Tile, bool> extraCondition = null)
+    {
+        extraCondition ??= _ => true;
+
+        selectableTilesForMovement.Add(origin);
+        var justAdded = new List<Tile>() {origin};
+        var iteration = 0;
+
+        origin.SetPathRing(iteration);
+
+        AddNeighbors();
+
+        foreach (var tile in selectableTilesForMovement)
+        {
+            tile.SetAppearance(Tile.Appearance.Selectable);
+        }
+
+        void AddNeighbors()
+        {
+            if (iteration >= range) return;
+
+            var neighbors = justAdded.SelectMany(tile => tile.GetDirectNeighbors(includeDiag)).Distinct().ToList();
+
+            var validTiles = neighbors
+                .Where(tile => !selectableTilesForMovement.Contains(tile))
+                .Where(extraCondition)
+                .ToList();
+
+            selectableTilesForMovement.AddRange(validTiles);
+            justAdded = validTiles;
+
+            iteration++;
+
+            foreach (var addedTile in justAdded)
+            {
+                addedTile.SetPathRing(iteration);
+            }
+
+            AddNeighbors();
+        }
+    }
+    
+    private void MoveUnit(Unit unit)
+    {
+        var destination = tileM.GetClickTile();
+
+        if (!selectableTilesForMovement.Contains(destination)) return;
+
+        var path = GetPathFromSelectableTiles(destination);
+
+        ResetTilesAppearance();
+
+        unit.MoveUnit(path);
+    }
+    
+    private List<Tile> GetPathFromSelectableTiles(Tile destination)
+    {
+        if (!selectableTilesForMovement.Contains(destination)) return null;
+            
+        var path = new List<Tile>() {destination};
+        var lastAdded = destination;
+
+        for (int i = destination.PathRing - 1; i >= 1; i--)
+        {
+            lastAdded = lastAdded.GetDirectNeighbors().FirstOrDefault(tile => tile.PathRing == i);
+            if (lastAdded == null) break;
+            path.Add(lastAdded);
+        }
+
+        path.Reverse();
+
+        return path;
     }
     
     
