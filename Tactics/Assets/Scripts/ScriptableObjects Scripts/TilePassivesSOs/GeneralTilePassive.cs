ca17f3fed8325a504ea13d1a2b580b6eafdb940d
Super Hubert <h.ramiandrasoa@rubika-edu.com>
2023-10-22 22:31:04 +0200

    Created General Tile Passive

---
+++ Tactics/Assets/Scripts/ScriptableObjects Scripts/TilePassivesSOs/GeneralTilePassive.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Battle.ScriptableObjects.Passive
+{
+    [CreateAssetMenu(menuName = "Battle Scriptables/Tile Passive/Basic Tile Passive")]
+    public class GeneralTilePassive : TilePassiveSo<Tile>
+    {
+        [SerializeField] private bool removeOnUnitExit = false;
+        [SerializeField] private bool removeOnUnitEnter = false;
+        [SerializeField] private int stacksModifierOnUnitEnter = 0;
+        [SerializeField] private int stacksModifierOnUnitExit = 0;
+        [SerializeField] private List<PassiveToAdd> passiveToAddOnUnitEnter = new List<PassiveToAdd>();
+        
+        protected override IEnumerator OnAddedEffect(Tile tile, PassiveInstance<Tile> instance)
+        {
+            if(removeOnUnitEnter || passiveToAddOnUnitEnter.Count > 0) tile.AddOnUnitEnterEvent(UnitEnterGeneralEffect(tile, instance));
+            if(removeOnUnitExit) tile.AddOnUnitExitEvent(UnitExitGeneralEffect(tile, instance));
+            yield break;
+        }
+
+        protected override IEnumerator OnRemovedEffect(Tile tile, PassiveInstance<Tile> instance)
+        {
+            RemoveEvents(tile,instance);
+            yield break;
+        }
+
+        protected IEnumerator UnitEnterGeneralEffect(Tile tile, PassiveInstance<Tile> instance)
+        {
+            foreach (var passiveToAdd in passiveToAddOnUnitEnter)
+            {
+                yield return tile.StartCoroutine(passiveToAdd.AddPassive(tile));
+            }
+            
+            if(stacksModifierOnUnitEnter != 0) yield return tile.StartCoroutine(instance.IncreaseStacks(stacksModifierOnUnitEnter));
+            
+            if(removeOnUnitEnter)
+            {
+                RemoveEvents(tile,instance);
+                yield return tile.StartCoroutine(tile.RemovePassiveEffect(instance));
+            }
+        }
+
+        protected IEnumerator UnitExitGeneralEffect(Tile tile, PassiveInstance<Tile> instance)
+        {
+            if(stacksModifierOnUnitExit != 0) yield return tile.StartCoroutine(instance.IncreaseStacks(stacksModifierOnUnitExit));
+            
+            if (removeOnUnitExit)
+            {
+                RemoveEvents(tile,instance);
+                yield return tile.StartCoroutine(tile.RemovePassiveEffect(instance));
+            }
+        }
+
+        private void RemoveEvents(Tile tile, PassiveInstance<Tile> instance)
+        {
+            if(removeOnUnitEnter) tile.RemoveOnUnitEnterEvent(UnitEnterGeneralEffect(tile, instance));
+            if(removeOnUnitExit) tile.RemoveOnUnitExitEvent(UnitExitGeneralEffect(tile, instance));
+        }
+
+        protected override IEnumerator UnitEnterEffect(Tile tile, PassiveInstance<Tile> instance)
+        {
+            yield break;
+        }
+
+        protected override IEnumerator UnitExitEffect(Tile tile, PassiveInstance<Tile> instance)
+        {
+            yield break;
+        }
+    }
+}
+
+
