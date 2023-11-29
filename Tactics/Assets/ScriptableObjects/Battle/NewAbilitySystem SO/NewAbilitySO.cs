60cafa7fa393bdea168b0cb15a40f4f95e2c4a30
hub <h.ramiandrasoa@rubika-edu.com>
2023-11-29 16:22:22 +0100

    cleanup

--- Tactics/Assets/ScriptableObjects/Battle/NewAbilitySystem SO/NewAbilitySO.cs
+++ Tactics/Assets/ScriptableObjects/Battle/NewAbilitySystem SO/NewAbilitySO.cs
@@ -1,9 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Battle.ScriptableObjects
 {
